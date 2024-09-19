ae1b14295485d9e7f8f60bde0e04904a56bc32be
Paul Lietar <pl2113@ic.ac.uk>
2024-09-19 13:14:34 +0100

    Go

--- run.R
+++ run.R
@@ -1,13 +1,11 @@
 url <- Sys.getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
 token <- Sys.getenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN")
 
-r <- httr2::request(url)
-print(r)
-r <- r |> httr2::req_url_query(audience="xxxx") |> httr2::req_auth_bearer_token(token)
-print(r)
-r <- r |> httr2::req_perform()
-print(r)
-oidc_token <- httr2::resp_body_json()$token
+oidc_token <- httr2::request(url) |>
+  httr2::req_url_query(audience="xxxx") |>
+  httr2::req_auth_bearer_token(token) |>
+  httr2::req_perform() |>
+  httr2::resp_body_json(r)$token
 
 r <- httr2::request("https://0618-82-132-214-24.ngrok-free.app") |>
   httr2::req_url_path("/auth/jwt") |>
