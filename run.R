2d36777fc64a746fc943f34d3905bf3a20a086c8
Paul Liétar <pl2113@ic.ac.uk>
2024-09-27 17:29:59 +0100

    Prod time

--- run.R
+++ run.R
@@ -1,3 +1,12 @@
+get_packit_audience <- function(base_url) {
+  print(httr2::request(base_url) |> httr2::req_url_path("packit/api/auth/login/service/audience"))
+  response <- httr2::request(base_url) |>
+    httr2::req_url_path("packit/api/auth/login/service/audience") |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+  response$audience
+}
+
 get_oidc_token <- function(audience) {
   url <- Sys.getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
   token <- Sys.getenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN")
@@ -12,16 +21,17 @@ get_oidc_token <- function(audience) {
 
 get_packit_token <- function(base_url, token) {
   response <- httr2::request(base_url) |>
-    httr2::req_url_path("/packit/api/auth/login/jwt") |>
+    httr2::req_url_path("packit/api/auth/login/service") |>
     httr2::req_body_json(list(token = token)) |>
     httr2::req_perform() |>
     httr2::resp_body_json()
   response$token
 }
 
-PACKIT_URL <- "https://0618-82-132-214-24.ngrok-free.app"
+PACKIT_URL <- "https://packit.dide.ic.ac.uk/reside"
 
-github_token <- get_oidc_token(audience = PACKIT_URL)
+audience <- get_packit_audience(PACKIT_URL)
+github_token <- get_oidc_token(audience)
 packit_token <- get_packit_token(PACKIT_URL, github_token)
 
 orderly2::orderly_init(force = TRUE)
