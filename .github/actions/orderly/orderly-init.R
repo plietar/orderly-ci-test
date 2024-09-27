2f4b1c3ddf3453b392591dea37f9b22d96caa18a
Paul Liétar <pl2113@ic.ac.uk>
2024-09-27 18:16:36 +0100

    Prod time

---
+++ .github/actions/orderly/orderly-init.R
@@ -0,0 +1,6 @@
+if (!requireNamespace("orderly2", quietly=TRUE)) {
+  install.packages("orderly2", repos = c("https://mrc-ide.r-universe.dev",
+                                         "https://cloud.r-project.org"))
+}
+orderly2::orderly_init(force = TRUE)
+
